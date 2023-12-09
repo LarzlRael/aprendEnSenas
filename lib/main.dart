b0e9a6ee0db87001c224603617a26042ebbc16af
Reynaldo GT <larz.dev@gmail.com>
2023-12-09 18:16:15 -0400

    firt commit

---
+++ lib/main.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/material.dart';
+
+import 'package:provider/provider.dart';
+import 'package:asl/provider/providers.dart';
+import 'package:asl/router/app_router.dart';
+import 'constants/constant.dart';
+import 'constants/enviroments.dart';
+
+void main() async {
+  await Enviroment.initEnviroment();
+  return runApp(
+    ChangeNotifierProvider(
+      create: (_) => ThemeProviderNotifier(),
+      child: const MyApp(),
+    ),
+  );
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final appTheme = Provider.of<ThemeProviderNotifier>(context).appTheme;
+    return MultiProvider(
+      providers: [
+        /* Fill this with another provider */
+      ],
+      child: MaterialApp.router(
+        title: appName,
+        debugShowCheckedModeBanner: false,
+        routerConfig: appRouter,
+        theme: appTheme.getTheme(),
+      ),
+    );
+  }
+}
