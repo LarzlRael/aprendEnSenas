b83e54cc4bb17670fd7c68f74b37c84e26fc4329
Reynaldo GT <larz.dev@gmail.com>
2024-01-23 17:53:28 -0400

    Update AndroidManifest.xml and send_message_input.dart

--- lib/forms/send_message_input.dart
+++ lib/forms/send_message_input.dart
@@ -1,63 +1,62 @@
 part of '../widgets/widgets.dart';
 
-const borderRadious = 20.0;
+const borderRadious = 40.0;
 
 class TextFieldSendMessage extends HookConsumerWidget {
   TextFieldSendMessage({super.key});
   @override
   Widget build(BuildContext context, WidgetRef ref) {
-    final controller = useTextEditingController()
+    final textController = useTextEditingController()
       ..value = TextEditingValue(
           text: ref.watch(signProviderProvider).currentMessage);
 
-    return SizedBox(
-      /* height: 45, */
-      child: TextField(
-        /* style: TextStyle(fontSize: 14.0, height: 1.0, color: Colors.black), */
-        minLines: 1,
-        maxLines: 2,
-        controller: controller,
-        decoration: InputDecoration(
-          /* isDense: true, // Added this */
-          contentPadding: EdgeInsets.symmetric(horizontal: 16),
-          suffix: AnimatedOpacity(
-            duration: const Duration(milliseconds: 500),
-            opacity:
-                ref.watch(signProviderProvider).currentMessage.isEmpty ? 0 : 1,
-            child: IconButton(
-              color: Colors.grey,
-              onPressed: () {
-                controller.clear();
-                ref.read(signProviderProvider.notifier).setCurrentMessage("");
-              },
-              icon: Icon(Icons.cancel),
-            ),
+    return Card(
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.circular(40),
+      ),
+      child: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 10),
+        child: TextField(
+          /* focusNode: focusNode,
+                 */
+          controller: textController,
+          minLines: 1,
+          maxLines: 3,
+          /*  style: textTheme.bodySmall!.copyWith(
+                  fontSize: fontSize,
+                  /* color: braileProvider.getPickerTextColor, */
+                  /* color: globalProviderS.pickerColor, */
+                  fontWeight: FontWeight.normal,
+                ), */
+
+          decoration: InputDecoration(
+            /* hintText: "Ingrese su texto aquí", */
+            /* contentPadding: EdgeInsets.symmetric(horizontal: 6), */
+            hintStyle: TextStyle(fontSize: 15),
+            border: InputBorder.none,
+            suffixIcon: textController.text.isNotEmpty
+                ? IconButton(
+                    onPressed: () {
+                      textController.text = "";
+                      textController.clear();
+
+                      /* ref.read(brailleProvider.notifier).setNormalText(""); */
+                    },
+                    icon: Icon(
+                      Icons.cancel,
+                    ),
+                  )
+                : null,
           ),
-          focusedBorder: OutlineInputBorder(
-            borderRadius: BorderRadius.circular(borderRadious),
-            borderSide: BorderSide(
-              color: Colors.orange,
-            ),
-          ),
-          /* filled: true,
-                        fillColor: Colors.black, */
-          border: OutlineInputBorder(
-            borderRadius: BorderRadius.circular(borderRadious),
-          ),
-          /* hintText: "Escribe tu mensaje",
-          hintStyle: TextStyle(
-            fontSize: 14,
-            fontWeight: FontWeight.w400,
-          ), */
-          labelText: "Escribe tu mensaje",
+          onChanged: (val) {
+            ref
+                .read(
+                  signProviderProvider.notifier,
+                )
+                .setCurrentMessage(val);
+            /* onTextChange(value); */
+          },
         ),
-        onChanged: (val) {
-          ref
-              .read(
-                signProviderProvider.notifier,
-              )
-              .setCurrentMessage(val);
-        },
       ),
     );
   }
